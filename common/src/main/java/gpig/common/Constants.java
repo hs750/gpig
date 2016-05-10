98939655f00449077de82ba6173caab4707a17b3
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-05-10 14:56:12 +0100

    Values -> Constants

---
+++ common/src/main/java/gpig/common/Constants.java
@@ -0,0 +1,5 @@
+package gpig.common;
+
+public class Constants {
+    public final static double DRONE_SPEED = 1.2; /* m/s */
+}
--- common/src/main/java/gpig/common/Values.java
+++
@@ -1,5 +0,0 @@
-package gpig.common;
-
-public class Values {
-    public final static double droneSpeed = 1.2;
-}
