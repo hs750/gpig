5e8100f0a3bff6402ae6b86e6e13a99b4c0e3321
Jonny Gilchrist <jonnygilchrist@gmail.com>
2016-05-12 16:00:19 +0100

    Implement Location/Area classes using SimpleLatLng

--- common/src/main/java/gpig/common/data/DeploymentArea.java
+++ common/src/main/java/gpig/common/data/DeploymentArea.java
@@ -1,21 +1,25 @@
 package gpig.common.data;
 
-import gpig.common.exceptions.UnimplementedException;
+import com.javadocmd.simplelatlng.window.CircularWindow;
 
 /**
  * The circle which represents the circle covered by a single deployment centre.
  */
 public class DeploymentArea {
 
-    public final Location location;
+    public final Location centre;
+    private final CircularWindow area;
 
-    public DeploymentArea(Location location) {
-        this.location = location;
+    public DeploymentArea(Location centre) {
+        this.centre = centre;
+        this.area = new CircularWindow(centre.location, Constants.DEPLOYMENT_RADIUS);
     }
 
-    public boolean contains(Location location) {
-        // TODO
-        throw new UnimplementedException();
+    public boolean contains(Location that) {
+        return area.contains(that.location);
     }
 
+    public boolean overlaps(DeploymentArea that) {
+        return area.overlaps(that.area);
+    }
 }
--- common/src/main/java/gpig/common/data/Location.java
+++ common/src/main/java/gpig/common/data/Location.java
@@ -1,15 +1,31 @@
 package gpig.common.data;
 
+import com.javadocmd.simplelatlng.LatLng;
+import com.javadocmd.simplelatlng.LatLngTool;
+import com.javadocmd.simplelatlng.util.LengthUnit;
+
 /**
  * A single point on a map, defined by latitude and longitude.
  */
 public class Location {
-    public final float latitude;
-    public final float longitude;
+    final LatLng location;
 
-    public Location(float latitude, float longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
+    public Location(double latitude, double longitude) {
+        location = new LatLng(latitude, longitude);
+    }
+
+    public double latitude() {
+        return location.getLatitude();
+    }
+
+    public double longitude() {
+        return location.getLongitude();
+    }
+
+    /* TODO: this should not return a double - type safe units will
+     * make confusion about units compile time errors */
+    public double distanceFrom(Location that, LengthUnit unit) {
+        return LatLngTool.distance(location, that.location, unit);
     }
 
     @Override
@@ -20,8 +36,6 @@ public class Location {
             return false;
 
         Location that = (Location) o;
-
-        return Float.compare(this.latitude, that.latitude) == 0 && Float.compare(this.longitude, that.longitude) == 0;
-
+        return location.equals(that.location);
     }
 }
