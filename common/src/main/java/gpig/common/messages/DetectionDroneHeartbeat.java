8068afa32c25bd437053bd63fe2018347e844e17
Harrison Spain <contact@harrison-spain.com>
2016-05-16 17:56:25 +0100

    detection drone heartbeat constructor

--- common/src/main/java/gpig/common/messages/DetectionDroneHeartbeat.java
+++ common/src/main/java/gpig/common/messages/DetectionDroneHeartbeat.java
@@ -6,6 +6,11 @@ public class DetectionDroneHeartbeat {
     public final Boolean deployed;
     public final UUID origin;
     
+    public DetectionDroneHeartbeat(UUID origin, boolean deployed){
+        this.deployed = deployed;
+        this.origin = origin;
+    }
+    
     private DetectionDroneHeartbeat() {
         deployed = null;
         origin = null;
