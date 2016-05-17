6f21195a3bb9ae4f3fdb529d6a6e7e7cd70c1c64
Harrison Spain <contact@harrison-spain.com>
2016-05-17 10:38:19 +0100

    refactored general purpose drone dispatching code to DroneDispatcher class.

--- dc/src/main/java/gpig/dc/dispatching/DetectionDroneDispatcher.java
+++ dc/src/main/java/gpig/dc/dispatching/DetectionDroneDispatcher.java
@@ -2,51 +2,24 @@ package gpig.dc.dispatching;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.UUID;
 
-import gpig.common.data.DroneState;
 import gpig.common.data.Location;
 import gpig.common.data.Path;
 import gpig.common.data.Path.Waypoint;
 import gpig.common.messages.DetectionDroneHeartbeat;
-import gpig.common.messages.SetPath;
 import gpig.common.messages.handlers.DetectionDroneHeartbeatHandler;
 import gpig.common.movement.RecoveryStrategy;
 import gpig.common.networking.MessageSender;
 import gpig.common.units.Kilometres;
 import gpig.common.util.Log;
 
-public class DetectionDroneDispatcher extends Thread implements DetectionDroneHeartbeatHandler {
+public class DetectionDroneDispatcher extends DroneDispatcher implements DetectionDroneHeartbeatHandler {
     private final Kilometres edgeDistance = new Kilometres(8.6);
-    private Location currentLocation;
-    private boolean deployable = false;
-    private boolean deployed = false;
 
-    private LinkedHashMap<UUID, DetectionDroneHeartbeat> allDrones;
-    private MessageSender messager;
-    private RecoveryStrategy recoveryStrategy;
-
-    public DetectionDroneDispatcher(MessageSender messager, RecoveryStrategy recoveryStrategy, Location currentLocation) {
-        this.messager = messager;
-        allDrones = new LinkedHashMap<>();
-        this.recoveryStrategy = recoveryStrategy;
-        this.currentLocation = currentLocation;
-    }
-
-    public void setCurrentLocation(Location location) {
-        currentLocation = location;
-    }
-
-    public void deployDrones(){
-        if(allDronesRecovered()){
-            deployed = false;
-        }
-        if (!deployable && !deployed) {
-            deployable = true;
-            start();
-        }
+    public DetectionDroneDispatcher(MessageSender messager, RecoveryStrategy recoveryStrategy,
+            Location currentLocation) {
+        super(messager, recoveryStrategy, currentLocation);
     }
 
     private ArrayList<Path> calculateSearchPattern() {
@@ -69,82 +42,21 @@ public class DetectionDroneDispatcher extends Thread implements DetectionDroneHe
 
     }
 
-    public synchronized void recoverDrones() {
-        deployable = false;
-
-        for (DetectionDroneHeartbeat drone : allDrones.values()) {
-            if (drone.state != DroneState.UNDEPLOYED) {
-                SetPath sp = new SetPath(recoveryStrategy.getPath(currentLocation), drone.origin);
-                messager.send(sp);
-            }
-        }
-
-    }
-
-    public synchronized boolean allDronesRecovered() {
-        boolean recovered = true;
-        for (DetectionDroneHeartbeat drone : allDrones.values()) {
-            if (drone.state != DroneState.UNDEPLOYED) {
-                recovered = false;
-            }
-        }
-        return recovered;
-    }
-
     @Override
     public void run() {
+        addTasks(calculateSearchPattern());
         super.run();
-        ArrayList<Path> searchPaths = calculateSearchPattern();
-        int searchsDeployed = 0;
-        while (deployable) {
-            while (allDronesActive()) {
-                try {
-                    Thread.sleep(1000); // Wait for a drone to become available
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            UUID assignee = getNextInactiveDrone();
-
-            SetPath sp = new SetPath(searchPaths.get(0), assignee);
-            messager.send(sp);
-            Log.info("Displatched Drone: %s", assignee);
-            searchsDeployed++;
-
-            if (searchsDeployed == searchPaths.size()) {
-                deployable = false;
-            }
-
-            activateDrone(assignee);
-        }
-    }
-
-    private synchronized boolean allDronesActive() {
-        for (DetectionDroneHeartbeat drone : allDrones.values()) {
-            if (drone.state == DroneState.UNDEPLOYED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private synchronized void activateDrone(UUID drone) {
-        allDrones.remove(drone); // remove the current heartbeat
-    }
-
-    private synchronized UUID getNextInactiveDrone() {
-        for (DetectionDroneHeartbeat drone : allDrones.values()) {
-            if (drone.state == DroneState.UNDEPLOYED) {
-                return drone.origin;
-            }
-        }
-        return null;
     }
 
     @Override
     public synchronized void handle(DetectionDroneHeartbeat message) {
-        allDrones.put(message.origin, message);
+        super.handle(message);
+    }
+
+    @Override
+    protected void taskListEmpty() {
+        deployable = false; // stop attempting to deploy drones when task done
+        Log.info("Detection sweep completed");
     }
 
 }
---
+++ dc/src/main/java/gpig/dc/dispatching/DroneDispatcher.java
@@ -0,0 +1,135 @@
+package gpig.dc.dispatching;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import gpig.common.data.DroneState;
+import gpig.common.data.Location;
+import gpig.common.data.Path;
+import gpig.common.messages.DroneHeartbeat;
+import gpig.common.messages.SetPath;
+import gpig.common.movement.RecoveryStrategy;
+import gpig.common.networking.MessageSender;
+import gpig.common.util.Log;
+
+public abstract class DroneDispatcher extends Thread {
+    protected Location currentLocation;
+    protected boolean deployable = false;
+    protected boolean deployed = false;
+
+    private LinkedHashMap<UUID, DroneHeartbeat> allDrones;
+    private MessageSender messager;
+    private RecoveryStrategy recoveryStrategy;
+
+    private ConcurrentLinkedQueue<Path> tasks;
+
+    public DroneDispatcher(MessageSender messager, RecoveryStrategy recoveryStrategy, Location currentLocation) {
+        this.messager = messager;
+        allDrones = new LinkedHashMap<>();
+        this.recoveryStrategy = recoveryStrategy;
+        this.currentLocation = currentLocation;
+        tasks = new ConcurrentLinkedQueue<>();
+    }
+
+    public void setCurrentLocation(Location location) {
+        currentLocation = location;
+    }
+
+    public void deployDrones() {
+        if (allDronesRecovered()) {
+            deployed = false;
+        }
+        if (!deployable && !deployed) {
+            deployable = true;
+            start();
+        }
+    }
+
+    public synchronized void recoverDrones() {
+        deployable = false;
+
+        for (DroneHeartbeat drone : allDrones.values()) {
+            if (drone.state != DroneState.UNDEPLOYED) {
+                SetPath sp = new SetPath(recoveryStrategy.getPath(currentLocation), drone.origin);
+                messager.send(sp);
+            }
+        }
+
+    }
+
+    public synchronized boolean allDronesRecovered() {
+        boolean recovered = true;
+        for (DroneHeartbeat drone : allDrones.values()) {
+            if (drone.state != DroneState.UNDEPLOYED) {
+                recovered = false;
+            }
+        }
+        return recovered;
+    }
+
+    protected synchronized boolean allDronesActive() {
+        for (DroneHeartbeat drone : allDrones.values()) {
+            if (drone.state == DroneState.UNDEPLOYED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected synchronized void activateDrone(UUID drone) {
+        allDrones.remove(drone); // remove the current heartbeat
+    }
+
+    protected synchronized UUID getNextInactiveDrone() {
+        for (DroneHeartbeat drone : allDrones.values()) {
+            if (drone.state == DroneState.UNDEPLOYED) {
+                return drone.origin;
+            }
+        }
+        return null;
+    }
+
+    public void handle(DroneHeartbeat heartbeat) {
+        allDrones.put(heartbeat.origin, heartbeat);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        while (deployable) {
+            while (allDronesActive()) {
+                try {
+                    Thread.sleep(1000); // Wait for a drone to become available
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            UUID assignee = getNextInactiveDrone();
+
+            Path p = tasks.poll();
+            if (p == null) {
+                taskListEmpty();
+            } else {
+                SetPath sp = new SetPath(p, assignee);
+                messager.send(sp);
+                Log.info("Displatched Drone: %s", assignee);
+
+                activateDrone(assignee);
+            }
+        }
+    }
+
+    protected void addTask(Path task) {
+        tasks.add(task);
+    }
+
+    protected void addTasks(Collection<Path> tasks) {
+        this.tasks.addAll(tasks);
+    }
+
+    protected abstract void taskListEmpty();
+}
