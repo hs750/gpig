b70039026aaac397bbcf726edc68406d8dd7295f
Harrison Spain <contact@harrison-spain.com>
2016-05-27 14:17:22 +0100

    when a drone changes from failed to alive there is a jump in locaiton which causes this locaiton to be null. This prevents C2 crash

--- c2/src/main/java/gpig/c2/gui/DetectionDroneInfoPanel.java
+++ c2/src/main/java/gpig/c2/gui/DetectionDroneInfoPanel.java
@@ -10,28 +10,31 @@ import gpig.common.data.DroneState;
 import gpig.common.data.Location;
 
 /**
- *	Displays information about a DC
+ * Displays information about a DC
  */
-public class DetectionDroneInfoPanel extends InfoPanel{
-	
-	private Location location;
-	private DroneState state;
-	
-	public DetectionDroneInfoPanel(UUID id, Location location, DroneState state, ActorType actorType, URL imageURL, Dimension size) {
-		super(actorType,imageURL, size);
-		this.location = location;
-		this.state = state;
-		
-		//Set parent field contents
-		actorIdL.setText(""+id);
-		actorTypeL.setText("Detection Drone");
-		actorStateL.setText(""+state);		
-		actorLatL.setText(""+String.format ("%.6f", location.latitude()));
-		actorLonL.setText(""+String.format ("%.6f", location.longitude()));
-	}
-	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-	}
+public class DetectionDroneInfoPanel extends InfoPanel {
+
+    private Location location;
+    private DroneState state;
+
+    public DetectionDroneInfoPanel(UUID id, Location location, DroneState state, ActorType actorType, URL imageURL,
+            Dimension size) {
+        super(actorType, imageURL, size);
+        this.location = location;
+        this.state = state;
+
+        // Set parent field contents
+        actorIdL.setText("" + id);
+        actorTypeL.setText("Detection Drone");
+        actorStateL.setText("" + state);
+        if (location != null) {
+            actorLatL.setText("" + String.format("%.6f", location.latitude()));
+            actorLonL.setText("" + String.format("%.6f", location.longitude()));
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 
 }
