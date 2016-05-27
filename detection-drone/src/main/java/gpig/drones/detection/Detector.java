2558250ec0f6fb292a55cb2635e0507aff29540b
Harrison Spain <contact@harrison-spain.com>
2016-05-27 20:38:38 +0100

    actually do detections. To reduce the chances of missing detections in large steps, detections are done every 10m in a line from the start of a step to the end. The victims file is read from config so that several victim location config files may be pre-made

---
+++ detection-drone/src/main/java/gpig/drones/detection/Detector.java
@@ -0,0 +1,58 @@
+package gpig.drones.detection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import gpig.common.data.Detection;
+import gpig.common.data.Location;
+import gpig.common.messages.DetectionNotification;
+import gpig.common.networking.MessageSender;
+import gpig.common.units.Kilometres;
+import gpig.common.util.Log;
+
+public class Detector {
+    // Detect every 10 meters along the path
+    private final static Kilometres detectionInterval = new Kilometres(0.01);
+    private DetectionBehaviour detBehaviour;
+    private MessageSender messenger;
+    private List<Detection> detections = new ArrayList<>();
+
+    public Detector(DetectionBehaviour detectionBehaviour, MessageSender messenger) {
+        this.detBehaviour = detectionBehaviour;
+        this.messenger = messenger;
+    }
+
+    public void detect(Location start, Location end) {
+        Kilometres distance = start.distanceFrom(end);
+        double bearing = start.bearingOf(end);
+        Kilometres detectDistance = new Kilometres(0);
+
+        detect(detBehaviour.getDetections(start));
+        detectDistance = detectDistance.add(detectionInterval);
+
+        while (detectDistance.value() <= distance.value()) {
+            Location l = start.locationAt(bearing, detectDistance);
+            detect(detBehaviour.getDetections(l));
+            detectDistance = detectDistance.add(detectionInterval);
+        }
+
+        detect(detBehaviour.getDetections(end));
+    }
+
+    private void detect(List<Detection> detections) {
+        detections.forEach(d -> detect(d));
+    }
+
+    private void detect(Detection detection) {
+        if (!hasBeenDetected(detection)) {
+            detections.add(detection);
+            messenger.send(new DetectionNotification(detection));
+            Log.info("Detected %s at %s", detection.person.type.toString(), detection.person.location.toString());
+        }
+    }
+
+    private boolean hasBeenDetected(Detection detection) {
+        boolean exists = detections.stream().anyMatch(det -> det.person.location.equals(detection.person.location));
+        return exists;
+    }
+}
--- detection-drone/src/main/java/gpig/drones/detection/config/DetectionDroneConfig.java
+++ detection-drone/src/main/java/gpig/drones/detection/config/DetectionDroneConfig.java
@@ -1,8 +1,11 @@
 package gpig.drones.detection.config;
 
+import java.io.File;
+
 import gpig.common.config.CommonConfig;
 import gpig.common.config.CommunicationChannelConfig;
 
 public class DetectionDroneConfig extends CommonConfig {
     public CommunicationChannelConfig dtdcChannel;
+    public File detectionConfig;
 }
